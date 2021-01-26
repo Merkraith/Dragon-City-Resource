4df2ffb7e91efe511dcdfeb1d52f2c478c8e994e
Mark Heil <mark.heil1122@gmail.com>
2021-01-26 10:29:54 -0800

    created navbar code

---
+++ src/components/navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Nav from 'react-bootstrap/Nav'
+
+
+function TopNavbar() {
+    return (
+        <div>
+            <Nav fill variant="tabs" defaultActiveKey="/home">
+                <Nav.Item>
+                    <Nav.Link href="/home">Active</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-2">Link</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                    <Nav.Link eventKey="link-3">Link</Nav.Link>
+                </Nav.Item>
+            </Nav>
+        </div>
+    );
+}
+
+export default TopNavbar;
