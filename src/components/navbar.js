a5f38d1189efeff62a4db11cd1ce92c07ee3d41b
Mark Heil <mark.heil1122@gmail.com>
2021-01-26 12:45:47 -0800

    navbar component edited

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,33 @@
 import React from "react";
 import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
 
 
 function TopNavbar() {
     return (
         <div>
-            <Nav fill variant="tabs" defaultActiveKey="/home">
-                <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-2">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                    <Nav.Link eventKey="link-3">Link</Nav.Link>
-                </Nav.Item>
-            </Nav>
+            <Container>
+                <Navbar expand="lg" variant="light" bg="light">
+                    <Nav variant="pills" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link href="/home">Active</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-1">Option 1</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-2">Option 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-3">Option 3</Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Navbar>
+            </Container>
         </div>
     );
 }
 
 export default TopNavbar;
+
