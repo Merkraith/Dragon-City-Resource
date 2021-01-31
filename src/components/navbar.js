5c393bd25fda125993ad7775404f3af643a00d39
Mark Heil <mark.heil1122@gmail.com>
2021-01-30 22:37:29 -0800

    more edits to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,30 +6,20 @@ import Navbar from 'react-bootstrap/Navbar';
 function TopNavbar() {
     return (
         <div>
-                {/* <Navbar expand="lg" variant="light" bg="light">
-                    <Nav variant="pills" defaultActiveKey="/home">
-                        <Nav.Item>
-                            <Nav.Link href="/home">Active</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-1">Option 1</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-2">Option 2</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-3">Option 3</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar> */}
-                <Navbar bg="dark" variant="dark">
+            <Navbar bg="dark" variant="dark">
+                <Nav className="container-fluid">
+                <Nav.Item>
                     <Navbar.Brand href="#home">Merkraith's Dragon City Resource</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav.Item>
+                    <Nav className="justify-content-end">
+                        <Nav.Link href="#Home">Home</Nav.Link>
+                        <Nav.Link href="#Gold">Gold Effiency</Nav.Link>
+                        <Nav.Link href="#Breeding">Breeding</Nav.Link>
+                        <Nav.Link href="#Strategies">Strategies</Nav.Link>
+                        <Nav.Link href="#Charts">Charts</Nav.Link>
                     </Nav>
-                </Navbar>
+                </Nav>
+            </Navbar>
         </div>
     );
 }
